bfe072cb2f83584e090b1d41f5f0ed74b0168312
Marcello Bardus <marcellobardus8@gmail.com>
2023-02-22 16:02:59 +0000

    fetching headers from rpc

--- single-chunk-processor/src/runner.py
+++ single-chunk-processor/src/runner.py
@@ -1,9 +1,15 @@
 import os
 import math
+import json
 import aiohttp
 import asyncio
 
 RPC_BATCH_MAX_SIZE = 50
+
+async def send_rpc_request(session: aiohttp.ClientSession, url, rpc_request):
+    async with session.post(url=url, data=json.dumps(rpc_request)) as response:
+        return await response.json()
+
 async def fetch_blocks_from_rpc(range_from: int, range_till: int, rpc_url: str):
     assert range_from > range_till, "Invalid range"
     number_of_blocks = range_from - range_till
@@ -16,29 +22,22 @@ async def fetch_blocks_from_rpc(range_from: int, range_till: int, rpc_url: str):
         requests = map(lambda j: {
             "jsonrpc": '2.0',
             "method": 'eth_getBlockByNumber',
-            "params": ['0x' + hex(range_from - (i - 1) * RPC_BATCH_MAX_SIZE - j), False],
+            "params": [hex(range_from - (i - 1) * RPC_BATCH_MAX_SIZE - j), False],
             "id": str(j)
         }, range(0, current_batch_size))
     
-        # Print the requests as a list
-        print(len(list(requests)))
-
+    async with aiohttp.ClientSession() as session:
+        tasks = [asyncio.ensure_future(send_rpc_request(session, rpc_url, request)) for request in requests]
+        results = await asyncio.gather(*tasks)
+        return results
+    
 
 async def main():
     from_block = int(os.environ.get('FROM_BLOCK'))
     till_block = int(os.environ.get('TILL_BLOCK'))
     rpc_url = os.environ.get('RPC_URL')
 
-    print(from_block)
-    print(till_block)
-    print(rpc_url)
-
-    await fetch_blocks_from_rpc(from_block, till_block, rpc_url)
-
-    # async with aiohttp.ClientSession() as session:
-    #     pokemon_url = 'https://pokeapi.co/api/v2/pokemon/151'
-    #     async with session.get(pokemon_url) as resp:
-    #         pokemon = await resp.json()
-    #         print(pokemon['name'])
+    results = await fetch_blocks_from_rpc(from_block, till_block, rpc_url)
+    print(results)
 
 asyncio.run(main())
