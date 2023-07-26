32d9a92042b444e226f555e0bd409ea9a69bdf41
feltroid Prime <feltroidprime@protonmail.com>
2023-07-26 14:00:16 +0200

    ✨ precompute outputs

--- tools/make/prepare_inputs_api.py
+++ tools/make/prepare_inputs_api.py
@@ -38,11 +38,16 @@ def rpc_request(url, rpc_request):
 
 
 def prepare_chunk_input(last_peaks:list, last_mmr_size:int, last_mmr_root:int, from_block_number_high:int, to_block_number_low) -> json:
-    data={}
+    chunk_input={}
+    chunk_output={}
     blocks = fetch_blocks_from_rpc_no_async(from_block_number_high+1, to_block_number_low-1, ALCHEMY_RPC)
     blocks.reverse()
     block_n_plus_one = blocks[-1]
-    parent_hash_input = split_128(int.from_bytes(bytes.fromhex(block_n_plus_one.parentHash.hex()[2:]), 'little'))
+    block_n_minus_r_plus_one = blocks[0]
+    block_n_plus_one_parent_hash_little = split_128(int.from_bytes(bytes.fromhex(block_n_plus_one.parentHash.hex()[2:]), 'little'))
+    block_n_plus_one_parent_hash_big = split_128(int.from_bytes(bytes.fromhex(block_n_plus_one.parentHash.hex()[2:]), 'big'))
+    block_n_minus_r_plus_one_parent_hash_big = split_128(int.from_bytes(bytes.fromhex(block_n_minus_r_plus_one.parentHash.hex()[2:]), 'big'))
+
 
     blocks = blocks[:-1]
     # print([x.number for x in blocks])
@@ -50,17 +55,24 @@ def prepare_chunk_input(last_peaks:list, last_mmr_size:int, last_mmr_root:int, f
     blocks_len = [len(block) for block in blocks]
     blocks = [bytes_to_little_endian_ints(block) for block in blocks]
 
-    data['mmr_last_root'] = last_mmr_root
-    data['mmr_last_len'] = last_mmr_size
-    data['mmr_last_peaks'] = last_peaks
-    data['from_block_number_high'] = from_block_number_high
-    data['to_block_number_low'] = to_block_number_low
-    data['block_n_plus_one_parent_hash_little_low'] = parent_hash_input[0]
-    data['block_n_plus_one_parent_hash_little_high'] = parent_hash_input[1]
-    data["rlp_arrays"] = blocks
-    data["bytes_len_array"] = blocks_len 
+    chunk_input['mmr_last_root'] = last_mmr_root
+    chunk_input['mmr_last_len'] = last_mmr_size
+    chunk_input['mmr_last_peaks'] = last_peaks
+    chunk_input['from_block_number_high'] = from_block_number_high
+    chunk_input['to_block_number_low'] = to_block_number_low
+    chunk_input['block_n_plus_one_parent_hash_little_low'] = block_n_plus_one_parent_hash_little[0]
+    chunk_input['block_n_plus_one_parent_hash_little_high'] = block_n_plus_one_parent_hash_little[1]
+    chunk_input["rlp_arrays"] = blocks
+    chunk_input["bytes_len_array"] = blocks_len 
 
-    return data
+    chunk_output['block_n_plus_one_parent_hash']=block_n_plus_one_parent_hash_big
+    chunk_output['block_n_minus_r_plus_one_parent_hash']=block_n_minus_r_plus_one_parent_hash_big
+    chunk_output['mmr_last_root'] = last_mmr_root
+    chunk_output['mmr_last_len'] = last_mmr_size
+
+
+                 
+    return chunk_input, chunk_output
 
 def chunk_process_api(last_peaks:list, last_mmr_size:int, from_block_number_high:int, to_block_number_low) -> json:
     data={}
@@ -94,8 +106,8 @@ def prepare_full_chain_inputs(from_block_number_high, batch_size=50):
     last_mmr_root = poseidon_hash(last_mmr_size, last_peaks[0])
 
     print(f"Preparing input for blocks from {from_block_number_high} to {to_block_number_low}")
-    
-    chunk_input = prepare_chunk_input(last_peaks, last_mmr_size, last_mmr_root, from_block_number_high, to_block_number_low)
+
+    chunk_input, chunk_output  = prepare_chunk_input(last_peaks, last_mmr_size, last_mmr_root, from_block_number_high, to_block_number_low)
     with open(f"{PATH}blocks_{from_block_number_high}_{to_block_number_low}_input.json", 'w') as f:
         json.dump(chunk_input, f, indent=4)
 
@@ -109,28 +121,43 @@ def prepare_full_chain_inputs(from_block_number_high, batch_size=50):
     if to_block_number_low==0:
         return
 
-
     while to_block_number_low >= 0:
+        print("start while", to_block_number_low)
         print(f"Preparing input for blocks from {from_block_number_high} to {to_block_number_low}")
         previous_chunk_data = chunk_process_api(last_peaks, last_mmr_size, previous_block_high, previous_block_low)
         last_peaks = previous_chunk_data['mmr_last_peaks']
         last_mmr_size = previous_chunk_data['mmr_last_len']
         last_mmr_root = previous_chunk_data['mmr_last_root']
+
+        chunk_output['new_mmr_root'] = last_mmr_root
+        chunk_output['new_mmr_len'] = last_mmr_size
+
+        print(f"Writing output for blocks from {previous_block_high} to {previous_block_low}")
+        with open(f"{PATH}blocks_{previous_block_high}_{previous_block_low}_output.json", 'w') as f:
+            json.dump(chunk_output, f, indent=4)
+
         
         time.sleep(0.5)
-        chunk_input = prepare_chunk_input(last_peaks, last_mmr_size, last_mmr_root, from_block_number_high, to_block_number_low)
+        chunk_input, chunk_output = prepare_chunk_input(last_peaks, last_mmr_size, last_mmr_root, from_block_number_high, to_block_number_low)
         with open(f"{PATH}blocks_{from_block_number_high}_{to_block_number_low}_input.json", 'w') as f:
             json.dump(chunk_input, f, indent=4)
         
         if to_block_number_low==0:
+            previous_chunk_data = chunk_process_api(last_peaks, last_mmr_size, from_block_number_high, to_block_number_low)
+            last_mmr_size = previous_chunk_data['mmr_last_len']
+            last_mmr_root = previous_chunk_data['mmr_last_root']
+            chunk_output['new_mmr_root'] = last_mmr_root
+            chunk_output['new_mmr_len'] = last_mmr_size
+            print(f"Writing output for blocks from {from_block_number_high} to {to_block_number_low}")
+            with open(f"{PATH}blocks_{from_block_number_high}_{to_block_number_low}_output.json", 'w') as f:
+                json.dump(chunk_output, f, indent=4)
             break
 
         previous_block_high = from_block_number_high
         previous_block_low = to_block_number_low
         from_block_number_high = to_block_number_low - 1
 
-
-        to_block_number_low = from_block_number_high - batch_size + 1 if from_block_number_high - batch_size + 1 >= 0 else 0
+        to_block_number_low = from_block_number_high - batch_size + 1 if from_block_number_high - batch_size + 1 >= 0 else 0    
 
 
         
